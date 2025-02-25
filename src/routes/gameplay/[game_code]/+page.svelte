86515435e9358c2b5ea692e7e9b93b2ef393ffea
Pieter Algera <pieteralgera@ymail.com>
2025-02-25 23:09:24 +0100

    Improved visuals, reactivite and  changed to  icons for the  x and o s

--- src/routes/gameplay/[game_code]/+page.svelte
+++ src/routes/gameplay/[game_code]/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+    import Icon from 'svelte-awesome';
+    import xmark from 'svelte-awesome/icons/close';
+    import circleO from 'svelte-awesome/icons/circleO';
 	import { goto } from '$app/navigation';
 	import { game_code_store, player_number_store } from '$lib/stores/LocalStorageHandler';
 	import type { User } from '$lib/types/User.js';
@@ -72,33 +75,31 @@
     }
 </script>
 
-<div class="flex flex-col gap-4 items-center justify-center">
-    <h1 class="text-2xl">Online tick tack toe</h1>
-    <h2>{gameInformation.player1} {current_player == 1? "X" : ""} VS {gameInformation.player2} {current_player == 2? "X" : ""}</h2>
-    <h3>Game code: {gameInformation.game_code}</h3>
-    {#if gameInformation.is_game_done}
-        <div class="{gameInformation.winner == parseInt($player_number_store)? "text-green-700" : "text-red-700"}">
-            {gameInformation.winner == parseInt($player_number_store)? "Winner" : "You lost"}
-        </div>
-    {/if}
-    <div class="grid grid-cols-3 grid-rows-3 w-100 h-100">
-        {#each slots as slot, index}
+<div class="flex w-full h-full">
+    <div class="flex flex-col flex-grow gap-4 items-center justify-center">
+        <h1 class="text-2xl">Online tick tack toe</h1>
+        <h2>{gameInformation.player1} {current_player == 1? "X" : ""} VS {gameInformation.player2} {current_player == 2? "X" : ""}</h2>
+        <h3>Game code: {gameInformation.game_code}</h3>
+        {#if gameInformation.is_game_done}
+            <div class="{gameInformation.winner == parseInt($player_number_store)? "text-green-700" : "text-red-700"}">
+                {gameInformation.winner == parseInt($player_number_store)? "Winner" : "You lost"}
+            </div>
+        {/if}
+        <div class="grid grid-cols-3 grid-rows-3 w-1/3 max-w-150 min-w-90 aspect-square">
+            {#each slots as slot, index}
             <!-- svelte-ignore a11y_click_events_have_key_events -->
             <!-- svelte-ignore a11y_no_static_element_interactions -->
-            <div class="flex bg-base-300 h-full items-center justify-center border-2" onclick={()=>{onclick(index)}}>
+            <div class="flex bg-cyan-100 h-full items-center justify-center border-2" onclick={()=>{onclick(index)}}>
                 {#if slot == "1"}
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.1 -0.1 3.2 3.2">
-                        <path d="M.5.5 2 2m-1.5 0L2 .5" stroke="#000000" stroke-width="0.3" fill="none"/>
-                    </svg>
+                <Icon data={xmark} scale={7}/>
                 {:else if slot == "2"}
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.1 -0.1 3.2 3.2">
-                        <circle cx="1.5" cy="1.5" r="1" stroke="black" stroke-width="0.3" fill="none" />
-                    </svg>
+                <Icon data={circleO} scale={7}/>
                 {/if}
             </div>
-        {/each}
+            {/each}
+        </div>
+        {#if gameInformation.is_game_done}
+            <button class="btn btn-primary" onclick={newGame}>New game</button>
+        {/if}
     </div>
-    {#if gameInformation.is_game_done}
-        <button class="btn btn-primary" onclick={newGame}>New game</button>
-    {/if}
 </div>
